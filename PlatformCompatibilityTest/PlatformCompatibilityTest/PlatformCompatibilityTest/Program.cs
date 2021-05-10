ece383bf7f827369be6537a231545725a01b761f
Juanjo <juanjo@jmontiel.es>
2021-05-10 20:45:10 +0200

    Añadido el proyecto de test PlatformCompatibilityTest.

---
+++ PlatformCompatibilityTest/PlatformCompatibilityTest/PlatformCompatibilityTest/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace PlatformCompatibilityTest
+{
+	public static class Program
+	{
+		static void Main()
+		{
+			var soundHandler = new WindowsSoundHandler();
+			if (OperatingSystem.IsWindows())
+			{
+				soundHandler.PlaySound(@"c:\sound.wav");
+			}
+			Console.WriteLine("Hello World!");
+		}
+	}
+}
---
+++ PlatformCompatibilityTest/PlatformCompatibilityTest/PlatformCompatibilityTest/WindowsSoundHandler.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Versioning;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlatformCompatibilityTest
+{
+	public class WindowsSoundHandler
+	{
+
+		[SupportedOSPlatform("windows")]
+		public void PlaySound(string soundPath)
+		{
+			Console.WriteLine($"Reproduciendo el sonido {soundPath}");
+		}
+	}
+}
