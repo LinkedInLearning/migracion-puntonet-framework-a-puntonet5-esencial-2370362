d0861da9c9eb72a248c6b75c30d69d89211f82a3
Juanjo <juanjo@jmontiel.es>
2021-05-12 23:57:46 +0200

    Aplicación MVC migrada a .Net 5. Capa de datos migrada a EFCore 5 (aunque aún faltaría el tema de migraciones e inicialización.

---
+++ eShopLegacyMVCSolution/src/eShopMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace eShopMVC.Models
+{
+	public class ErrorViewModel
+	{
+		public string RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
