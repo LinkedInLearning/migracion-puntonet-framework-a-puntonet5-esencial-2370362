882ce4742010be5b2e199d504bb34ced4e0f7d2d
Juanjo <juanjo@jmontiel.es>
2021-05-15 13:17:08 +0200

    Añado apis en ambos proyectos para explicar las diferencias de enrutamiento.

---
+++ eShopLegacyMVCSolution/src/eShopLegacyMVC/Api/CatalogController.cs
@@ -0,0 +1,30 @@
+﻿using eShopLegacyMVC.Models;
+using eShopLegacyMVC.Services;
+using eShopLegacyMVC.ViewModel;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace eShopLegacyMVC.Api
+{
+	public class CatalogController : ApiController
+	{
+		private readonly ICatalogService _catalogService;
+
+		public CatalogController(ICatalogService catalogService)
+		{
+			_catalogService = catalogService;
+		}
+
+		[HttpGet]
+		public PaginatedItemsViewModel<CatalogItem> GetItems(int pageSize = 10, int pageIndex = 1)
+		{
+			return _catalogService.GetCatalogItemsPaginated(pageSize, pageIndex);
+		}
+
+	}
+}
---
+++ eShopLegacyMVCSolution/src/eShopMVC/Api/CatalogController.cs
@@ -0,0 +1,33 @@
+﻿using eShop.Data;
+
+using eShopMVC.Services;
+using eShopMVC.ViewModel;
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eShopMVC.Api
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class CatalogController : ControllerBase
+	{
+		private readonly ICatalogService _catalogService;
+
+		public CatalogController(ICatalogService catalogService)
+		{
+			_catalogService = catalogService;
+		}
+
+		[HttpGet]
+		public PaginatedItemsViewModel<CatalogItem> GetItems(int pageSize = 10, int pageIndex = 1)
+		{
+			return _catalogService.GetCatalogItemsPaginated(pageSize, pageIndex);
+		}
+	}
+}
