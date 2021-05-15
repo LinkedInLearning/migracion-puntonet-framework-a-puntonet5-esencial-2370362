f40be8dbbd0837e7d50c3f363950b6a0d2d3293a
Juanjo <juanjo@jmontiel.es>
2021-05-16 01:40:38 +0200

    Creadas dos aplicaciones de plantilla de Identity, una con ASP.Net MVC 5 y otra con ASP.Net MVC Core (scafolding), para explicar sus diferencias.

---
+++ IdentityWithMvc5/IdentityWithMvcCore/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using IdentityWithMvcCore.Areas.Identity.Data;
+using IdentityWithMvcCore.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(IdentityWithMvcCore.Areas.Identity.IdentityHostingStartup))]
+namespace IdentityWithMvcCore.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<IdentityCoreContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("IdentityCoreContextConnection")));
+
+                services.AddDefaultIdentity<IdentityCoreUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<IdentityCoreContext>();
+            });
+        }
+    }
+}
